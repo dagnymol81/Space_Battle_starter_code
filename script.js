2b9d958febf86649dd636d6d0ed78496ce7bc384
unknown <dagnymol@gmail.com>
2022-09-11 14:42:32 -0400

    comments and format

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+//define DOM elements
 let playerStats = document.querySelector(".playerStats")
 let enemyStats = document.querySelector(".enemyStats")
+
+//ship class
 class Ship {
   constructor(hull, firepower, accuracy) {
     this.hull = hull
@@ -7,7 +10,7 @@ class Ship {
     this.accuracy = accuracy
   }
 
-shoot(enemy) {
+  shoot(enemy) {
     if (Math.random() < this.accuracy) {
       enemy.hull -= this.firepower
       console.log("Hit!")
@@ -24,6 +27,7 @@ function randomInt(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+//function to alien ships
 function createAlienShip() {
   let hull = randomInt(3, 6)
   let firepower = randomInt(2, 4)
@@ -33,28 +37,31 @@ function createAlienShip() {
 
 function createAlienArray(numShips) {
   let arr = []
-    for (let i = 0; i < numShips; i++) {
-      arr.push(createAlienShip())
-    }
+  for (let i = 0; i < numShips; i++) {
+    arr.push(createAlienShip())
+  }
   return arr;
   }
 
+//function to update the DOM
 function updateStats() {
   playerStats.textContent = `Hull: ${USSHelloWorld.hull} Firepower: ${USSHelloWorld.firepower} Accuracy: ${USSHelloWorld.accuracy}`
   enemyStats.textContent = `Hull: ${alienArray[0].hull} Firepower: ${alienArray[0].firepower} Accuracy: ${alienArray[0].accuracy}`
 }
 
+//create ships
 let alienArray = createAlienArray(6)
 let USSHelloWorld = new Ship(20, 5, .7)
 
 //Start Game after timeout
 setTimeout(() => {
-  let startMessage = window.confirm('The fate of Earth is in your hands. Are you ready?');
+  let startMessage = window.confirm('Alien ships are attacking. Are you ready?');
   if (startMessage) {
     playGame()
   }
 }, '1000');
 
+//Player ship shoots, then alien ship (if it survives)
 function playRound() {
   if (USSHelloWorld.hull > 0) {
     USSHelloWorld.shoot(alienArray[0]) //Player shoots if they have hull remaining
@@ -64,32 +71,34 @@ function playRound() {
   }
 }
 
+//write inital stats
 updateStats()
 
+//Gameplay function. Loops by calling itself at the end if the ending condition is not met
 function playGame() {
 
-updateStats()
+  //update the DOM first
+  updateStats()
+  playRound() //both ships fire once if they have hull
 
-playRound() //both ships fire once if they have hull
-
- if (USSHelloWorld.hull <= 0) {
-    console.log("you lose!")
+  if (USSHelloWorld.hull <= 0) {
+    console.log("you lose!") //end if player ship has 0 hull
     return
-  } else if (alienArray[0].hull <= 0) {
-    alienArray.shift()
+  } else if (alienArray[0].hull <= 0) { 
+    alienArray.shift() //if alien ship has zero hull, move on to the next one
     console.log(alienArray.length + " ships remaining")
     if (alienArray.length < 1) {
-      console.log("you win!")
+      console.log("you win!") //if there are no alien ships remaining, end
       return
     } else {
       retreatMsg = window.confirm('Alien ship defeated. Continue?') 
       if (!retreatMsg) {
         console.log("retreat")
-        return
+        return //end if retreat
       }
     }
   }
 
-  setTimeout(playGame, 1000)
+  setTimeout(playGame, 1000) //give DOM time to update, then play again
 
 }
